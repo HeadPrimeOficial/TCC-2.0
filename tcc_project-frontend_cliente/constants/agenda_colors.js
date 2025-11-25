3782d6aa2dc570883015e6dc041bb4156a46df14
HeadPrimeOficial <headtutoriall@gmail.com>
2025-11-25 18:23:30 -0300

    MAPA OK

---
+++ tcc_project-frontend_cliente/constants/agenda_colors.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  background: '#8c8c91', // Cinza texturizado do fundo (aproximado)
+  primaryText: '#FFFFFF',
+  status: {
+    disponivel: '#66cc66', // Verde
+    emProcesso: '#ffdd66', // Amarelo
+    indisponivel: '#ff6666', // Vermelho
+  },
+  button: {
+    background: '#d68910', // Laranja/Dourado
+    text: '#FFFFFF',
+    border: '#f5b041',
+  },
+  navBar: '#d68910',
+};
